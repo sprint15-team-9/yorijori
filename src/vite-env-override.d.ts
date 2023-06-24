864dacd041f163ff9ebbb71571b4bee4cddb97ed
메리 <shigatsu970704@gmail.co>
2023-06-24 14:34:38 +0900

    chore: husky, lint-staged 설정

--- src/vite-env-override.d.ts
+++ src/vite-env-override.d.ts
@@ -1,5 +1,4 @@
 declare module '*.svg' {
-    const content: React.FC<React.SVGProps<SVGElement>>
-    export default content
-  }
-  
+  const content: React.FC<React.SVGProps<SVGElement>>;
+  export default content;
+}
