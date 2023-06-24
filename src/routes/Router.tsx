70f9612b36610ddb15a3e6121d9d3c79b2e13c16
hootre <zxv7295@naver.com>
2023-06-25 03:26:50 +0900

    chore : change supabase

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from '../pages/Home';
 import Detail from '../pages/Detail';
+import Supabase from '../pages/Supabase';
 
 export default function Router() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Home />,
+    },
+    {
+      path: '/supabase',
+      element: <Supabase />,
     },
     {
       path: '/detail/:id',
