267bf9102552a4a9bda831b8dd77312527b9bf97
solmin <solgo123@gmail.com>
2023-06-25 00:40:10 +0900

    chore: add Pretendard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider, styled } from 'styled-components';
 import Router from './routes/Router';
 import theme from './style/theme';
+import GlobalStyle from './style/GlobalStyle';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Wrapper>
         <Container>
           <Router />
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>IM HOME</div>;
+  return (
+    <div>
+      <p style={{ fontWeight: 'bold' }}>
+        asd p q asjhcnas 안녕하세요 스프린트 9조입니다.
+      </p>
+    </div>
+  );
 }
