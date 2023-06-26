7059cef7b4bd431221b1ea563391ff2936758ec5
Circlewee <pnj1118@naver.com>
2023-06-26 22:42:10 +0900

    fix: og image를 index.html에 추가

--- src/lib/SEO/SEOMetatags.tsx
+++ src/lib/SEO/SEOMetatags.tsx
@@ -16,10 +16,6 @@ const MetaTag = () => {
         property="og:description"
         content="요리 초보자부터 고수까지 모두 사용할 수 있는 만능 레시피 서비스"
       />
-      <meta
-        property="og:image"
-        content="https://sprint-9.s3.ap-northeast-2.amazonaws.com/assets/ogImage.png"
-      />
       <meta property="og:image:type" content="image/png" />
     </Helmet>
   );
